59aee7352d9453206de1833a702ba58c8aced7e5
Serkan Kızılırmak <serkankizilirmaak@gmail.com>
2025-05-30 19:50:38 +0300

    v5

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/Serkankizilirmak/resume/', // Github Pages için tam repo adıyla base ayarı
+      base: '/Serkankizilirmak/resume/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
